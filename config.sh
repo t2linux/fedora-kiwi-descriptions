1e74928eb0719b1cb3548434a58d9ffd0c4d21fd
Neal Gompa <ngompa@fedoraproject.org>
2025-11-28 12:19:24 -0500

    components/desktops/kde: Replace Anaconda Initial Setup with Plasma Setup
    Plasma Setup is an out of box setup experience service
    that is now included by default with KDE Plasma.
    
    Reference: https://fedoraproject.org/wiki/Changes/Unified_KDE_OOBE

--- config.sh
+++ config.sh
@@ -120,7 +120,7 @@ fi
 #--------------------------------------
 
 if [[ "$kiwi_profiles" == *"Disk"* ]]; then
-	if [[ "$kiwi_profiles" != *"GNOME"* ]] && [[ "$kiwi_profiles" != *"COSMIC"* ]]; then
+	if [[ "$kiwi_profiles" != *"GNOME"* ]] && [[ "$kiwi_profiles" != *"KDE"* ]] && [[ "$kiwi_profiles" != *"COSMIC"* ]]; then
 		## Enable initial-setup
 		systemctl enable initial-setup.service
 		## Enable reconfig mode
