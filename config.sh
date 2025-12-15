e29d4d2f8fa184231a56f85babde756889e73193
Neal Gompa <ngompa@fedoraproject.org>
2025-12-14 19:43:53 -0500

    Add initial definitions for the Design Suite lab
    These are ported over from fedora-kickstarts with some small
    cleanups.

--- config.sh
+++ config.sh
@@ -296,6 +296,65 @@ if [[ "$kiwi_profiles" == *"Container"* ]] || [[ "$kiwi_profiles" == *"FEX"* ]];
 	fi
 fi
 
+if [[ "$kiwi_profiles" == *"Design_suite"* ]]; then
+# Add link to lists of tutorials
+cat >> /usr/share/applications/list-design-tutorials.desktop << FOE
+[Desktop Entry]
+Name=List of design tutorials
+GenericName=List of Tutorials for Designers
+Comment=Reference of Design Related Tutorials
+Exec=xdg-open https://fedoraproject.org/wiki/Design_Suite/Tutorials
+Type=Application
+Icon=applications-graphics
+Categories=Graphics;Documentation;
+FOE
+chmod a+x /usr/share/applications/list-design-tutorials.desktop
+
+# Add information about Fedora Design Suite
+cat >> /usr/share/applications/fedora-design-suite.desktop << FOE
+[Desktop Entry]
+Name=Design Suite Info
+GenericName=About Design Suite
+Comment=Wiki page of Design Suite
+Exec=xdg-open https://fedoraproject.org/wiki/Design_Suite
+Type=Application
+Icon=applications-internet
+Categories=Documentation;
+FOE
+chmod a+x /usr/share/applications/fedora-design-suite.desktop
+
+# Add information about Fedora Design Team
+cat >> /usr/share/applications/fedora-design-team.desktop << FOE
+[Desktop Entry]
+Name=Design Team Info
+GenericName=About Design Team
+Comment=Wiki page of Design Team
+Exec=xdg-open https://fedoraproject.org/wiki/Design
+Type=Application
+Icon=applications-internet
+Categories=Documentation;
+FOE
+chmod a+x /usr/share/applications/fedora-design-team.desktop
+
+# Use Powerline in bash
+cat >>  $HOME/.bashrc << FOE
+# Enable powerline daemon
+if [ -f `which powerline-daemon` ]; then
+  powerline-daemon -q
+  POWERLINE_BASH_CONTINUATION=1
+  POWERLINE_BASH_SELECT=1
+  . /usr/share/powerline/bash/powerline.sh
+fi
+FOE
+# End powerline override
+
+#Override the favorite desktop application in Dash
+sed -i "s/favorite-apps=."'*'"/favorite-apps=['org.mozilla.firefox.desktop', 'shotwell.desktop', 'gimp.desktop', 'darktable.desktop','krita.desktop', 'inkscape.desktop', 'blender.desktop', 'libreoffice-writer.desktop', 'scribus.desktop', 'pitivi.desktop', 'nautilus.desktop', 'bijiben.desktop', 'anaconda.desktop', 'list-design-tutorials.desktop']/" /usr/libexec/liveys/sessions.d/livesys-gnome
+
+# rebuild schema cache with any overrides we installed
+glib-compile-schemas /usr/share/glib-2.0/schemas
+fi
+
 if [[ "$kiwi_profiles" == *"SoaS"* ]]; then
 # Get proper release naming in the control panel
 cat >> /boot/olpc_build << EOF
